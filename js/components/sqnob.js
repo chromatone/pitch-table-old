2199db8ff4aacd32b453f40923a864f31f8cb7c3
DeFUCC <fond@fondfrukt.ru>
2020-04-08 23:46:58 +0300

    Square knob optimized for multitouch and further upgrades

--- js/components/sqnob.js
+++ js/components/sqnob.js
@@ -1,30 +1,104 @@
 export default {
   template: `
   <div
-    @mousedown.prevent="activate"
-    @touchstart.prevent="activate" @dblclick="reset()" class="sqnob">
-
+    @mousedown.prevent="mouseDown"
+    @touchstart.prevent="activate"
+    @dblclick="reset()"
+    class="sqnob">
     <div class="sqnob-info">
       {{value | round}}&nbsp;{{unit}}<br>
       {{param}}
     </div>
-
     <div class="sqnob-value" :style="{height:internalValue+'%'}"></div>
-
   </div>
   `,
   props: ["max", "min", "value", "step", "param","unit","log"],
   data() {
     return {
-      internalValue: this.mapNumber(this.value, this.min, this.max, 0, 100),
+      internalValue: this.mapInput(this.value),
+      initialValue: this.mapInput(this.value),
       logValue: 0,
       active: false,
       initialX: undefined,
       initialY: undefined,
       initialDragValue: undefined,
       shiftPressed: false,
-      initialValue: this.mapNumber(this.value, this.min, this.max, 0, 100)
+      activeTouch:undefined,
     };
+  },
+  watch: {
+    value: function(newVal) {
+      this.internalValue = this.mapInput(newVal)
+    }
+  },
+  filters: {
+    round(val) {
+      return Math.floor(val * 10) / 10;
+    }
+  },
+  methods: {
+    mouseDown(ev) {
+			this.initialX=ev.pageX;
+			this.initialY=ev.pageY;
+      this.active = true;
+      this.initialDragValue = this.internalValue;
+			document.onmousemove = this.mouseMove;
+			document.onmouseup = this.mouseUp;
+		},
+		mouseMove(ev) {
+			let {pageY} = ev;
+			let diff = 2;
+      if (this.shiftPressed) { diff=10 }
+      this.internalValue = this.initialDragValue + (this.initialY - pageY) / diff;
+      if (this.internalValue > 100) this.internalValue = 100;
+      if (this.internalValue < 0) this.internalValue = 0;
+      if (isNaN(this.internalValue)) this.internalValue = this.initialDragValue;
+      this.$emit( "input", this.mapOutput(this.internalValue) );
+		},
+		mouseUp(ev){
+			document.onmouseup = undefined;
+      document.onmousemove = undefined;
+      this.active = false;
+		},
+    reset() {
+      this.internalValue=this.initialValue;
+      this.$emit("input", this.mapOutput(this.internalValue) );
+    },
+    mapInput(value) {
+      return mapNumber(value, this.min, this.max, 0, 100, this.step)
+    },
+    mapOutput(value) {
+      return mapNumber(value, 0, 100, this.min, this.max, this.step)
+    },
+    activate(ev) {
+      this.activeTouch = ev.changedTouches[0].identifier
+      this.initialY = ev.changedTouches[0].pageY;
+      this.active = true;
+      this.initialDragValue = this.internalValue;
+      document.addEventListener("touchend", this.deactivate);
+      document.addEventListener("touchmove", this.dragHandler);
+    },
+    dragHandler(ev) {
+      let theTouch;
+      for (let touch of ev.changedTouches) {
+        if (touch.identifier==this.activeTouch) {
+          theTouch=touch
+        }
+      }
+      let {pageY} = theTouch;
+      let diff = 2;
+      if (this.shiftPressed) { diff=10 }
+      this.internalValue = this.initialDragValue + (this.initialY - pageY) / diff;
+      if (this.internalValue > 100) this.internalValue = 100;
+      if (this.internalValue < 0) this.internalValue = 0;
+      if (isNaN(this.internalValue)) this.internalValue = this.initialDragValue;
+      this.$emit( "input", this.mapOutput(this.internalValue) );
+    },
+    deactivate() {
+      document.removeEventListener("touchmove", this.dragHandler);
+      document.removeEventListener("touchend", this.deactivate);
+      this.active = false;
+    }
   },
   created() {
     document.addEventListener("keydown", e => {
@@ -34,76 +108,17 @@ export default {
       if (e.key == "Shift") this.shiftPressed = false;
     });
   },
-  watch: {
-    value: function(newVal, oldVal) {
-      this.internalValue = this.mapNumber(newVal, this.min, this.max, 0, 100);
-    }
-  },
-  filters: {
-    round(val) {
-      return Math.floor(val * 10) / 10;
-    }
-  },
-  computed: {
-    knobRotation() {
-      let rotation = this.mapNumber(this.internalValue, 0, 100, 0, 270) - 135;
-      return rotation;
-    }
-  },
-  methods: {
-    reset() {
-      this.internalValue=this.initialValue;
-      this.$emit(
-        "input",
-        this.mapNumber(this.internalValue, 0, 100, this.min, this.max)
-      );
-    },
-    mapNumber(value, inputmin=0, inputmax=100, rangemin=0, rangemax=100) {
-      rangemax = parseFloat(rangemax);
-      rangemin = parseFloat(rangemin);
-      inputmax = parseFloat(inputmax);
-      inputmin = parseFloat(inputmin);
-      let result =
-        (value - inputmin) * (rangemax - rangemin) / (inputmax - inputmin) + rangemin;
-
-      return result * (this.step || 100) / (this.step || 100);
-    },
-    activate(event) {
-      this.initialX = event.pageX || event.changedTouches[0].pageX;
-      this.initialY = event.pageY || event.changedTouches[0].pageY;
-      this.active = true;
-      this.initialDragValue = this.internalValue;
-      document.onmouseup = this.deactivate;
-      document.addEventListener("touchend", this.deactivate);
-      document.onmousemove = this.dragHandler;
-      document.addEventListener("touchmove", this.dragHandler);
-    },
-    dragHandler(e) {
-      let xLocation = e.pageX || e.changedTouches[0].pageX;
-      let yLocation = e.pageY || e.changedTouches[0].pageY;
-
-      if (this.shiftPressed) {
-        this.internalValue =
-          this.initialDragValue + (this.initialY - yLocation) / 10;
-      } else {
-        this.internalValue =
-          this.initialDragValue + (this.initialY - yLocation) / 2;
-      }
-
-      if (this.internalValue > 100) this.internalValue = 100;
-      if (this.internalValue < 0) this.internalValue = 0;
-      if (isNaN(this.internalValue)) this.internalValue = this.initialDragValue;
-      this.$emit(
-        "input",
-        this.mapNumber(this.internalValue, 0, 100, this.min, this.max)
-      );
-    },
-    deactivate() {
-      document.onmouseup = undefined;
-      document.onmousemove = undefined;
-      document.removeEventListener("touchmove", this.dragHandler);
-      document.removeEventListener("touchend", this.deactivate);
-      this.active = false;
-    }
+  beforeDestroy() {
+    document.removeEventListener('keydown')
+    document.removeEventListener('keyup')
   }
+}
+
+function mapNumber(value, inputmin=0, inputmax=100, rangemin=0, rangemax=100, step) {
+  rangemax = parseFloat(rangemax);
+  rangemin = parseFloat(rangemin);
+  inputmax = parseFloat(inputmax);
+  inputmin = parseFloat(inputmin);
+  let result = (value - inputmin) * (rangemax - rangemin) / (inputmax - inputmin) + rangemin;
+  return result * (step || 100) / (step || 100);
 }
